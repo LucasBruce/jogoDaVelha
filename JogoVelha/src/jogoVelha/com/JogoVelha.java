f11090cfa3cd6e0437b1873b8c978967a18f72da
Lucas Bruce <lucasmatheusbruce@gmail.com>
2018-09-25 10:43:31 -0300

    commit update

--- JogoVelha/src/jogoVelha/com/JogoVelha.java
+++ JogoVelha/src/jogoVelha/com/JogoVelha.java
@@ -17,7 +17,7 @@ public class JogoVelha{
 		int jogada = 1, linha = 0, coluna = 0;
 		char sinal;
 		
-		while(ganhou) {
+		while(!ganhou) {
 			
 			if(jogada % 2 == 1) {
 				System.out.println("Vez do jogador 1, entre com a linha e coluno(1-3).");
@@ -28,28 +28,30 @@ public class JogoVelha{
 			}
 			
 			linhaValida = false;
-			while(linhaValida) {
+			while(!linhaValida) {
 				System.out.println("Entre com a linha (1-3).");
 				linha = scan.nextInt();
 				
 				if((linha < 1 || linha > 3) ) {
-					linhaValida = true;
-				}else {
 					System.out.println("Linha invalida, tente novamente!");
 					linhaValida = false;
+					
+				}else {
+					linhaValida = true;
 				}
 			}
 			colunaValida = false;
-			while(colunaValida) {
+			while(!colunaValida) {
 			
 			System.out.println("Entre com a coluna (1-3).");
 			coluna = scan.nextInt();
 			
 			if(coluna < 1 || coluna > 3) {
-				colunaValida = true;
-			}else {
 				System.out.println("coluna invalida, tente novamente.");
 				colunaValida = false;
+				
+			}else {
+				colunaValida = true;
 			}
 			}
 			linha--;
@@ -66,6 +68,34 @@ public class JogoVelha{
 						System.out.print(jogoVelha[i][j]+" | ");
 					}
 					System.out.println();
+				}
+				
+				//avaliar se tem vencedor
+				if((jogoVelha[0][0] == 'X' && jogoVelha[0][1] == 'X' && jogoVelha[0][2] == 'X') ||
+				   (jogoVelha[1][0] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[1][2] == 'X') ||
+				   (jogoVelha[2][0] == 'X' && jogoVelha[2][1] == 'X' && jogoVelha[2][2] == 'X') ||
+				   (jogoVelha[0][0] == 'X' && jogoVelha[1][0] == 'X' && jogoVelha[2][0] == 'X') ||
+				   (jogoVelha[0][1] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[2][1] == 'X') ||
+				   (jogoVelha[0][2] == 'X' && jogoVelha[1][2] == 'X' && jogoVelha[2][2] == 'X') ||
+				   (jogoVelha[0][0] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[2][2] == 'X') ||
+				   (jogoVelha[0][2] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[2][0] == 'X')) {
+					System.out.println("Parabens o jogador 1 ganhou!");
+					ganhou = true;
+					
+				  }else if((jogoVelha[0][0] == 'O' && jogoVelha[0][1] == 'O' && jogoVelha[0][2] == 'O') ||
+						   (jogoVelha[1][0] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[1][2] == 'O') ||
+						   (jogoVelha[2][0] == 'O' && jogoVelha[2][1] == 'O' && jogoVelha[2][2] == 'O') ||
+						   (jogoVelha[0][0] == 'O' && jogoVelha[1][0] == 'O' && jogoVelha[2][0] == 'O') ||
+						   (jogoVelha[0][1] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[2][1] == 'O') ||
+						   (jogoVelha[0][2] == 'O' && jogoVelha[1][2] == 'O' && jogoVelha[2][2] == 'O') ||
+						   (jogoVelha[0][0] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[2][2] == 'O') ||
+						   (jogoVelha[0][2] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[2][0] == 'O')) {
+					  System.out.println("Parabens o jogador 2 ganhou!");
+					  ganhou = true;
+					  
+				}else if(jogada > 9) {
+					System.out.println("Ninguem ganhou a partida !");
+					ganhou = true;
 				}
 			}
 		}
